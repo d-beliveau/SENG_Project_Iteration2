d41b3107f935d0b544fb9fd59424f5a1374b2fda
deji4545 <oshoad3000@gmail.com>
2022-03-23 16:28:16 -0600

    Update Membership Card

--- SCS - Software/src/controller/MembershipCard.java
+++ SCS - Software/src/controller/MembershipCard.java
@@ -1,22 +1,50 @@
 package controller;
 
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+
+import javax.smartcardio.Card;
+
+import org.lsmr.selfcheckout.Barcode;
 import org.lsmr.selfcheckout.Card.CardData;
 import org.lsmr.selfcheckout.devices.AbstractDevice;
 import org.lsmr.selfcheckout.devices.CardReader;
 import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
 import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
 import org.lsmr.selfcheckout.devices.observers.CardReaderObserver;
+import org.lsmr.selfcheckout.products.BarcodedProduct;
+import org.lsmr.selfcheckout.Card;
 
 public class MembershipCard implements CardReaderObserver{
 
 	
 	private SelfCheckoutStation station;
 	
+	//Map card number to loyalty point
+	private  HashMap<String, Integer> loyaltyPointMap = new HashMap<>();
+	private String cardNumber;
 	
 	public MembershipCard(SelfCheckoutStation station) {
 		this.station = station;
 		station.cardReader.attach(this);
 	}
+	
+	//Set the hash table of loyalty point map
+	public void  setLoyaltyPointMap(HashMap<String, Integer> map) {
+		loyaltyPointMap = map;
+	}
+	
+	//Set customer points
+	public void setPoints(String cardNum, int points) {
+		loyaltyPointMap.put(cardNum, points);
+	}
+	
+	//Get the customer points
+	public int getPoints() {
+		return loyaltyPointMap.get(cardNumber);
+	}
+	
 	
 	@Override
 	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
@@ -32,32 +60,35 @@ public class MembershipCard implements CardReaderObserver{
 
 	@Override
 	public void cardInserted(CardReader reader) {
-		// TODO Auto-generated method stub
+		// Ignore
 		
 	}
 
 	@Override
 	public void cardRemoved(CardReader reader) {
-		// TODO Auto-generated method stub
-		
+		//Ignore
 	}
 
 	@Override
 	public void cardTapped(CardReader reader) {
-		// TODO Auto-generated method stub
+		// Ignore
 		
 	}
 
 	@Override
 	public void cardSwiped(CardReader reader) {
-		// TODO Auto-generated method stub
+		// Ignore
 		
 	}
 
 	@Override
 	public void cardDataRead(CardReader reader, CardData data) {
-		// TODO Auto-generated method stub
-		
+		//Retrieve the card number
+		if(data.getType().equals("Membership")) {
+			cardNumber = data.getNumber();
+		}
 	}
+	
+	
 
 }
