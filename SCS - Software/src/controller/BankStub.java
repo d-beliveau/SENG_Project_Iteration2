79b551ee71530657ed214e67a54ae9109d968c14
devont9122 <devon.tran@ucalgary.ca>
2022-03-25 02:05:59 -0600

    Update BankStub.java

--- SCS - Software/src/controller/BankStub.java
+++ SCS - Software/src/controller/BankStub.java
@@ -1,7 +1,23 @@
 package controller;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Random;
+
+import org.lsmr.selfcheckout.Card.CardData;
+
 public class BankStub {
-	private int availableFunds;
+	private BigDecimal availableFunds;
+	private double creditLimit;
+	
+	
+	public BigDecimal getCreditRemaining(CardData data) {
+		//obtain information from third party financial institution
+		availableFunds = new BigDecimal(((new Random().nextDouble() * (creditLimit)) + 0.01));
+		availableFunds.setScale(2);
+		
+		return availableFunds;
+	}
 	/*
 	 * Needed utility:
 	 * Credit limit
