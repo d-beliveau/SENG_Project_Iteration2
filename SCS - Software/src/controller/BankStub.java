799804df39331826ba60d5ba1edc39b624696904
devont9122 <devon.tran@ucalgary.ca>
2022-03-25 15:57:46 -0600

    fixed remaining funds calculation

--- SCS - Software/src/controller/BankStub.java
+++ SCS - Software/src/controller/BankStub.java
@@ -1,24 +1,31 @@
 package controller;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Random;
 
+import org.lsmr.selfcheckout.Card;
 import org.lsmr.selfcheckout.Card.CardData;
 
 public class BankStub {
 	private BigDecimal availableFunds;
-	private double creditLimit;
+	private double creditLimit = 1500;
 	
+	
+	
+	private Card card = new Card("debit", "1732785", "Person", "890","7483", true, true);
+	
+	public BankStub() {}
 	
 	public BigDecimal getCreditRemaining(CardData data) {
 		//obtain information from third party financial institution
 		
 		//obtaining funds remaining
-		availableFunds = new BigDecimal(((new Random().nextDouble() * (creditLimit)) + 0.01));
-		availableFunds.setScale(2);
-		
+		availableFunds = new BigDecimal(((new Random().nextDouble() * (1500)) + 0.01)).setScale(2, RoundingMode.HALF_DOWN);
 		return availableFunds;
 	}
+
 	
 	/*
 	 * Needed utility:
@@ -26,4 +33,9 @@ public class BankStub {
 	 * Debit funds
 	 */
 	
+
+
+
 }
+
+
