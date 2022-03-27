30e2d495ad0ac17011bc95968c0262bbb707ae58
devont9122 <devon.tran@ucalgary.ca>
2022-03-26 18:41:22 -0600

    Update BankStub.java

--- SCS - Software/src/controller/BankStub.java
+++ SCS - Software/src/controller/BankStub.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.HashMap;
 import java.util.Random;
 
 import org.lsmr.selfcheckout.Card;
@@ -12,16 +13,46 @@ public class BankStub {
 	private BigDecimal availableFunds;
 	private double creditLimit;
 	
+	private  HashMap<String , Double> creditLimitMap = new HashMap<>();
+	private  HashMap<String , BigDecimal> creditFundsAvailableMap = new HashMap<>();
+
+
+	
 	
 	public BankStub() {}
 	
-	public BigDecimal getCreditRemaining(CardData data) {
+	public void setCreditLimit(String cardNumber, double limit) {
+		creditLimitMap.put(cardNumber, limit);
+	}
+	
+	public void setAvailableFunds(String cardNumber, BigDecimal funds) {
+		creditFundsAvailableMap.put(cardNumber, funds);
+	}
+	
+	public double getCreditLimit(String cardNumber) {
+		return creditLimitMap.get(cardNumber);
+	}
+	
+	public BigDecimal getAvailableFunds(String cardNumber) {
+		return creditFundsAvailableMap.get(cardNumber);
+	}
+	
+	//to get a randomized amount of funds available
+	public BigDecimal getFundsRemaining(String cardNumber) {
 		//obtain information from third party financial institution
 		
 		//obtaining funds remaining
 		availableFunds = new BigDecimal(((new Random().nextDouble() * (creditLimit)) + 0.01)).setScale(2, RoundingMode.HALF_DOWN);
+		creditFundsAvailableMap.put(cardNumber, availableFunds);
 		return availableFunds;
 	}
+	
+	public void makePurchase(String cardNumber, BigDecimal amount) {
+		availableFunds.subtract(amount);
+	}
+	
+
+	
 
 	
 	/*
